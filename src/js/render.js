b0effcf3793311877d57ddbc40ec796746dca6b5
Tom Najdek <tom@doppnet.com>
2016-06-15 01:50:27 +0100

    Use correct escaping when preparing export. Fix #44

--- src/js/render.js
+++ src/js/render.js
@@ -237,7 +237,7 @@ ZoteroRenderer.prototype.prepareExport = function(itemEl) {
 		'group': false
 	}).then(item => {
 		let itemData = (_.findWhere || _.find)(this.data.raw, {'key': itemId});
-		let encoded = window.btoa(item.raw[0][exportFormat]);
+		let encoded = window.btoa(unescape(encodeURIComponent(item.raw[0][exportFormat])));
 		exportEl.classList.remove('zotero-loading-inline');
 		exportEl.innerHTML = exportTpl({
 			'filename': itemData.data.title + '.' + this.zotero.config.exportFormats[exportFormat].extension,
