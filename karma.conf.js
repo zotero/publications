fa7058cc385cd0b3697cf12e20cfc229e39dfcea
Tom Najdek <tom@doppnet.com>
2020-09-01 16:24:39 +0200

    Update travis config

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chromium'],
+		browsers: ['Chrome'],
 		singleRun: false,
 		concurrency: 1
 	});
